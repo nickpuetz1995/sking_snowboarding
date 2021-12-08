cadc7475f5cf4ecacc38622026194ad469cfc16d
anonymous anonymous <nick.puetz@kellogg.northwestern.edu>
2021-12-08 19:23:24 +0000

    Whitelist columns in NonSkiReview for ActiveAdmin

--- app/admin/non_ski_reviews.rb
+++ app/admin/non_ski_reviews.rb
@@ -1,5 +1,7 @@
 ActiveAdmin.register NonSkiReview do
 
+ permit_params :non_ski_activity_id, :user_id, :review, :rating
+
   # See permitted parameters documentation:
   # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
   #
