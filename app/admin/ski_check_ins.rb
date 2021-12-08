df0f79f1cbdf80a2ba67644766ffa6bc430eab63
anonymous anonymous <nick.puetz@kellogg.northwestern.edu>
2021-12-08 19:23:17 +0000

    Whitelist columns in SkiCheckIn for ActiveAdmin

--- app/admin/ski_check_ins.rb
+++ app/admin/ski_check_ins.rb
@@ -1,5 +1,7 @@
 ActiveAdmin.register SkiCheckIn do
 
+ permit_params :user_id, :ski_area_id
+
   # See permitted parameters documentation:
   # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
   #
