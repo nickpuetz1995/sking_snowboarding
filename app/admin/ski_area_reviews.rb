12ad6b4254784550e173d625f505f10df0797583
anonymous anonymous <nick.puetz@kellogg.northwestern.edu>
2021-12-08 19:23:19 +0000

    Whitelist columns in SkiAreaReview for ActiveAdmin

--- app/admin/ski_area_reviews.rb
+++ app/admin/ski_area_reviews.rb
@@ -1,5 +1,7 @@
 ActiveAdmin.register SkiAreaReview do
 
+ permit_params :user_id, :ski_area_id, :review, :rating
+
   # See permitted parameters documentation:
   # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
   #
